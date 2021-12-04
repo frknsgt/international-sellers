7b9687e1702c3a9a7f5cfedb094abee991ea2cec
Furkan Söğüt <48659931+frknsgt@users.noreply.github.com>
2021-12-04 08:29:26 +0300

    feat: added some connections

--- src/app/components/layouts/client/client-layout/client-layout.component.ts
+++ src/app/components/layouts/client/client-layout/client-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import { LanguageService } from 'src/app/utils';
 
 @Component({
   selector: 'app-client-layout',
@@ -6,9 +7,16 @@ import { Component, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./client-layout.component.scss'],
 })
 export class ClientLayoutComponent implements OnInit {
-  constructor(
+  constructor(private _languageService: LanguageService,
   ) {}
-
+  lang: string =
+  this._languageService.getLanguage() == 'en'
+    ? 'us'
+    : this._languageService.getLanguage() || 'tr';
   ngOnInit(): void {
+  }
+  setLang(lang: string) {
+    this.lang = lang == 'en' ? 'us' : lang;
+    this._languageService.setLanguage(lang);
   }
 }
--- src/app/pages/client/shop/shop.component.ts
+++ src/app/pages/client/shop/shop.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models';
+import { CategoryService } from 'src/app/utils/services/category/category.service';
 import { ProductService } from 'src/app/utils/services/product/product.service';
 
 @Component({
@@ -11,12 +12,24 @@ export class ShopComponent implements OnInit {
   
 
   constructor(
-    private _productService: ProductService
+    private _productService: ProductService,
+    private _categoryService: CategoryService
   ) { }
   
   product:any;
+  categories:any;
+  tempProduct:any;
   async ngOnInit() {
     this.product=<Array<Product>> await this._productService.listAsync();
+    this.categories=<Array<any>> await this._categoryService.listAsync();
+    console.log(this.categories)
+    this.tempProduct=this.product
+  }
+
+  filterData(Id?){
+    this.product=this.tempProduct
+    if(Id)
+    this.product=this.product.filter(data=>data.CategoryID==Id)
   }
 
 }
