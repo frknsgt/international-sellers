8c486d5472b45b5a201c3196cd03d58af9dddc95
ferhatosanoglu <ferhatosanogluo@gmail.com>
2021-12-04 15:14:09 +0300

    create learn service

--- src/app/utils/services/learn/learn.service.ts
+++ src/app/utils/services/learn/learn.service.ts
@@ -1,56 +1,55 @@
 import { Injectable } from '@angular/core';
+import { ApiFetchService } from '../api-fetch/api-fetch.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { CurrentUserService } from '../current-user/current-user.service';
-import { NotificationService } from '../notification/notification.service';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class LearnService {
+
   constructor(
     private _http: HttpClient,
-    private _currentUserService: CurrentUserService,
-    private _notificationService: NotificationService
   ) { }
 
+
   requestAsync(
-    method = 'GET',
-    data?: object,
+    method: string,
+    path: string,
+    data: object,
     getToken: boolean = false
   ) {
     return new Promise((resolve, reject) => {
       let config: object = {};
-      if (data != null)
-        if (method === 'GET') Object.assign(config, { params: data });
-        else Object.assign(config, { body: data });
+      if (data != null) Object.assign(config, { body: data });
       if (getToken)
         Object.assign(config, {
-          headers: {
-            token: this._currentUserService.value.token,
-          },
+          headers: { token: JSON.parse(localStorage.getItem('currentUser')!).token },
         });
 
       this._http
-        .request<any>('GET', 'http://127.0.0.1:5000/users', config)
+        .request<any>(method, `http://127.0.0.1:5000/${path}`, config)
         .subscribe(
           (res) => resolve(res),
-          (err) => {
-            if (err.status == 417)
-              this._notificationService.danger(
-                'Make sure you send the data correctly!'
-              );
-            else if (err.status >= 400 && err.status < 500)
-              this._notificationService.danger(err.message);
-            else
-              this._notificationService.danger(
-                'Server error occurred, please try again later If the error persists, we ask you to report this to the authorities'
-              );
+          (error) =>
             reject({
-              status: err.status,
-              message: err.error != undefined ? err.error.message : err.message,
-            });
-          }
+              status: error.status,
+              message:
+                error.error != undefined
+                  ? error.error.message
+                  : error.message,
+            })
         );
     });
+  }
+
+  async listAsync() {
+    return await this.requestAsync(
+      'GET',
+      'users',
+      null!,
+    );
   }
 }
